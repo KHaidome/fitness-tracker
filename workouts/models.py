470fb647c77295e0d40ca6d0fe45fdbfc80acc2e
KHaidome <obichi7@gmail.com>
2024-09-14 16:58:18 +0100

    Create Models to store basic data

--- workouts/models.py
+++ workouts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Workout(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s workout on {self.date}"
+
+class WorkoutExercise(models.Model):
+    workout = models.ForeignKey(Workout, on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE)
+    sets = models.IntegerField()
+    reps = models.IntegerField()
+    weight = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.exercise.name} - {self.sets}x{self.reps}"
