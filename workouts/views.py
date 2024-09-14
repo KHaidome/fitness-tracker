754a675a50d2cf34ee14bf5365743c025926652b
KHaidome <obichi7@gmail.com>
2024-09-14 17:00:06 +0100

    Create Views to define app logic

--- workouts/views.py
+++ workouts/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Workout, Exercise, WorkoutExercise
+from django import forms
 
-# Create your views here.
+class WorkoutForm(forms.ModelForm):
+    class Meta:
+        model = Workout
+        fields = ['date', 'notes']
+
+class WorkoutExerciseForm(forms.ModelForm):
+    class Meta:
+        model = WorkoutExercise
+        fields = ['exercise', 'sets', 'reps', 'weight']
+
+@login_required
+def workout_list(request):
+    workouts = Workout.objects.filter(user=request.user).order_by('-date')
+    return render(request, 'workouts/workout_list.html', {'workouts': workouts})
+
+@login_required
+def add_workout(request):
+    if request.method == 'POST':
+        form = WorkoutForm(request.POST)
+        if form.is_valid():
+            workout = form.save(commit=False)
+            workout.user = request.user
+            workout.save()
+            return redirect('workout_detail', workout_id=workout.id)
+    else:
+        form = WorkoutForm()
+    return render(request, 'workouts/add_workout.html', {'form': form})
+
+@login_required
+def workout_detail(request, workout_id):
+    workout = Workout.objects.get(id=workout_id, user=request.user)
+    exercises = WorkoutExercise.objects.filter(workout=workout)
+    
+    if request.method == 'POST':
+        form = WorkoutExerciseForm(request.POST)
+        if form.is_valid():
+            workout_exercise = form.save(commit=False)
+            workout_exercise.workout = workout
+            workout_exercise.save()
+            return redirect('workout_detail', workout_id=workout.id)
+    else:
+        form = WorkoutExerciseForm()
+    
+    return render(request, 'workouts/workout_detail.html', {
+        'workout': workout,
+        'exercises': exercises,
+        'form': form
+    })
